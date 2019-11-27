522b606df569ca12b9a63efa4b4b7a8224269705
Roman <thetrusty@gmail.com>
2019-11-27 19:05:03 +0300

    add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'theme.dart';
+
 void main() => runApp(App());
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.light(),
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: Styles.scaffoldBackgroundColor,
+      ),
       home: Container(),
     );
   }
---
+++ lib/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const Color scaffoldBackgroundColor = Color(0xff399099);
+
+  static const Color resetButtonColor = Color(0xffef6f6c);
+}
