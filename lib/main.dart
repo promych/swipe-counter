a2368a4e722e1d4fc3622b289b45c07b1f78b6db
Roman <thetrusty@gmail.com>
2019-11-27 19:10:28 +0300

    set portrait only

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/home/home.dart';
 import 'theme.dart';
 
-void main() => runApp(App());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(App());
+}
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.light().copyWith(
         scaffoldBackgroundColor: Styles.scaffoldBackgroundColor,
       ),
