c353175edbcdae1ff45bfb34d769d064e00a6700
Roman <thetrusty@gmail.com>
2019-11-28 15:45:39 +0300

    move repo up

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../data/local_storage.dart';
 import 'home_bloc.dart';
 import 'ui/app_title.dart';
 import 'ui/reset_button.dart';
 import 'ui/swipe_area.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   final CounterBloc bloc;
 
   const HomePage({Key key, @required this.bloc}) : super(key: key);
 
   static Widget create(BuildContext context) {
+    final repo = LocalStorage();
     return Provider<CounterBloc>(
-      create: (_) => HomeBloc()..load(),
+      create: (_) => HomeBloc(repo: repo)..load(),
       dispose: (_, bloc) => bloc.dispose(),
       child: Consumer<CounterBloc>(
         builder: (context, bloc, _) => HomePage(bloc: bloc),
@@ -21,11 +23,6 @@ class HomePage extends StatefulWidget {
     );
   }
 
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/screens/home/home_bloc.dart
+++ lib/screens/home/home_bloc.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
+
 import '../../constants.dart';
 import '../../data/local_storage.dart';
 
 abstract class CounterBloc {
   int get counter;
   Stream<int> get $counter;
+  void load();
   void increase();
   void decrease();
   void reset();
@@ -13,9 +16,11 @@ abstract class CounterBloc {
 }
 
 class HomeBloc implements CounterBloc {
-  CounterRepo _repository;
-  int _counter;
+  final CounterRepo repo;
 
+  HomeBloc({@required this.repo});
+
+  int _counter;
   @override
   int get counter => _counter;
 
@@ -23,13 +28,9 @@ class HomeBloc implements CounterBloc {
   @override
   Stream<int> get $counter => _counterController.stream;
 
-  Future<int> get savedValue async {
-    _repository = LocalStorage();
-    return await _repository.value;
-  }
-
+  @override
   void load() async {
-    await savedValue.then((value) {
+    await repo.value.then((value) {
       _counter = value;
       _counterController.sink.add(_counter);
     });
@@ -44,19 +45,19 @@ class HomeBloc implements CounterBloc {
   void decrease() async {
     _counter--;
     _counterController.sink.add(_counter);
-    await _repository.saveValue(_counter);
+    await repo.saveValue(_counter);
   }
 
   @override
   void increase() async {
     _counter++;
     _counterController.sink.add(_counter);
-    await _repository.saveValue(_counter);
+    await repo.saveValue(_counter);
   }
 
   void reset() async {
     _counter = kDefaultCounterValue;
     _counterController.sink.add(_counter);
-    await _repository.saveValue(_counter);
+    await repo.saveValue(_counter);
   }
 }
